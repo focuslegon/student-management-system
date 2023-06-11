07fb8beaf6fbf8f622898b7b2d7ad036880a0e7b
focuslegon <33376436+focuslegon@users.noreply.github.com>
2023-06-11 23:13:30 +0000

    changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import {ResultContextProvider} from './contexts/ResultsContexProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    {/* <ResultContextProvider> */}
        <App />
-     {/* </ResultContextProvider> */}
   </Router>
  
 );
